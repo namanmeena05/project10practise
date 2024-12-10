e897528ba73464ffed472f301572a6618497c56b
Naman Meena <139106924+namanmeena05@users.noreply.github.com>
2024-12-10 15:16:16 +0530

    Update ProjectOrsApplication.java

--- projectORS/src/main/java/com/rays/ProjectOrsApplication.java
+++ projectORS/src/main/java/com/rays/ProjectOrsApplication.java
@@ -23,62 +23,36 @@ import com.rays.common.FrontCtl;
 @SpringBootApplication
 public class ProjectOrsApplication extends SpringBootServletInitializer{
 
-	@Autowired
-	private Environment env;
+	
 
 	@Autowired
-	FrontCtl frontCtl;
-	
+	private FrontCtl frontCtl;
+
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectOrsApplication.class, args);
+
+		SpringApplication.run(SpringBootFrontCtlApplication.class, args);
+
 	}
 
-	/**       
-	 * Enables CORS to all urls
-	 * 
-	 * @return
-	 */        
-	
-	//  bean  annotation is method level annotation ,spring container manage the bean 
-	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 
 		WebMvcConfigurer w = new WebMvcConfigurer() {
 
-			/**
-			 * Add CORS
-			 */
 			public void addCorsMappings(CorsRegistry registry) {
 				CorsRegistration cors = registry.addMapping("/**");
 				cors.allowedOrigins("http://localhost:4200");
-				
-				//   ("*") = allows get and post method
 				cors.allowedHeaders("*");
 				cors.allowCredentials(true);
-			}   
-			/**
-			 * Add Interceptors
-			 */
-		
-			/*
-			 * @Override public void addInterceptors(InterceptorRegistry registry) {
-			 * registry.addInterceptor(frontCtl).addPathPatterns("/**").excludePathPatterns(
-			 * "/Auth/**"); }
-			 */
-			  
-			  
-			/*
-			 * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
-			 * registry.addResourceHandler("/**").addResourceLocations("classpath:/public/")
-			 * ; }
-			 * 
-			 */ 
-			  };
+			}
 
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(frontCtl).addPathPatterns("/**").excludePathPatterns("/Auth/**");
+			}
+		};
 		return w;
 	}
-	
 
 }
 	
